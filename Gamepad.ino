101f0f21c683c13ee35e1a50c22f439fd171b897
magnolia345 <your_email@example.com>
2026-09-07 11:38:35 -0400

    Resolved duplicate setup and loop functions

--- Gamepad.ino
+++ Gamepad.ino
@@ -124,12 +124,5 @@ void draw(){
       color = BLUE;
     }
 }
-void setup() {
-  // put your setup code here, to run once:
 
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
